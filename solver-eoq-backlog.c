cb00c124b6a7bb86ee8549344c5bf11fc3d6cae3
sofialeall01 <sofiamariaanaclarajleal@gmail.com>
2026-06-11 20:38:33 -0300

    atualizaçoes feitas em 08/06

--- solver-eoq-backlog.c
+++ solver-eoq-backlog.c
@@ -3,7 +3,7 @@ integrantes:
 Camily Leal Silva -0117239
 Felipe de Castro Leal -0117037
 Gabriel Mendonça de Oliveira -0117251
-Sofia Maria de Jesus Leal -
+Sofia Maria de Jesus Leal -120376
 
 */
 
@@ -45,26 +45,71 @@ double MetodoDaBisseccao(Parametros p, double a, double b, double epsilon, int m
         x = (a + b) / 2;
         DeltaX = (b - a) / 2;
 
-        if (fabs(DeltaX)<=epsilon && (fabs(EquacaoDerivadaParcial(p,x))<=epsilon))
+        if (fabs(DeltaX) <= epsilon && (fabs(EquacaoDerivadaParcial(p, x)) <= epsilon))
         {
             return x;
-        } else if (EquacaoDerivadaParcial(p,a)*EquacaoDerivadaParcial(p,x)>0)
-        {
-            a=x;
-        } else
-        {
-            b=x;
         }
-        
-        iter++;
+        else if (EquacaoDerivadaParcial(p, a) * EquacaoDerivadaParcial(p, x) > 0)
+        {
+            a = x;
+        }
+        else
+        {
+            b = x;
+        }
 
-        
+        iter++;
     }
     return x;
 }
 
-int main(int argc, char const *argv[])
+double EquacaoPolinomio(Parametros p, double q)
+{
+    return (((p.h / 2.0) * (1 - (p.h / (p.h + p.pi)))) * q * q - p.d * p.s);
+}
+
+int Tabelamento(int *a, int *b, int passo, Parametros p)
 {
 
+    int q = a;
+    int q_prox = q + passo;
+
+    while (q < b)
+    {
+        double fx = EquacaoPolinomio(p, q);
+        double fx_prox = EquacaoPolinomio(p, q_prox);
+
+        if (fx * fx_prox < 0)
+        {
+            return 1;
+        }
+
+        q = q_prox;
+        q_prox = q + passo;
+    }
+}
+
+double EquacaoB_Isolado(Parametros p, double q)
+{
+    return (q * (p.h / (p.h + p.pi)));
+}
+
+double EquacaoCustoTotal(Parametros p, double q, double b)
+{
+    return (((p.d / q) * p.s) + (pow((q - b), 2) / 2 * q) * p.h + (pow(b, 2) / 2 * q) * p.pi);
+}
+int main(int argc, char const *argv[])
+{
+    if (argc != 5)
+    {
+        printf("Numero de parametros invalido, programa encerrando");
+        return 0;
+    }
+
+    Parametros p;
+    p.d = atof(argv[1]);
+    p.h = atof(argv[2]);
+    p.s = atof(argv[3]);
+    p.pi = atof(argv[4]);
     return 0;
 }
